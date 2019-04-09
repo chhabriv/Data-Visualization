d9e9c4b7e58d70995fcd4ae4a800d46a1565fe62
Viren Chhabria <chhabriv@tcd.ie>
2019-04-09 22:40:58 +0100

    Final Changes

--- barcelona-dataset-anaysis/BarcelonaScript.R
+++ barcelona-dataset-anaysis/BarcelonaScript.R
@@ -2,9 +2,10 @@ library(readr)
 library(ggmap)
 library(geojsonio)
 library(dplyr)
-library(networkD3)
 library(broom)
 library(sp)
+library(viridis)
+library(reshape2)
 
 LEFT=2.11
 RIGHT=2.22
@@ -19,13 +20,12 @@ ACCIDENTS="barcelona-accidents-2017.csv"
 WEALTH="barcelona-terretorial-income-2017.csv"
 UNEMPLOYMENT="barcelona-unemployment-2017.csv"
 ACCIDENT_VS_PEDESTRIAN_TITLE="2017 - Pedestrian Accidents vs Pedestrian Crossings in Barcelona"
-DISTRICT_PEDESTRIAN_COUNT="2017 - Pedestrian Accidents per District in Barcelona"
+DISTRICT_PEDESTRIAN_COUNT="2017 - Number of Pedestrian Accidents and Crossings per District in Barcelona"
 PREDESTRIAN_CAUSE_COUNT="2017 - Pedestrian Accidents Cause in Barcelona"
 INCOME_UNEMPLOYMENT_DISTRICT="2017 - District wise Wealth Vs Unemployment Rate"
-DAY_HOUR_COUNT_1="(1) 2017 - Hourly count of accidents per day of the week"
-DAY_HOUR_COUNT_2="(2) 2017 - Hourly count of accidents per day of the week"
-WEALTH_DISTRIBUTION_HEATMAP="2017 - Wealth Distribution heatmap per Neighborhood in Barcelona"
-
+DAY_HOUR_COUNT="2017 - Hourly count of accidents per day of the week"
+WEALTH_DISTRIBUTION_HEATMAP="2017 - Wealth Distribution in Barcelona - Neighborhood Heatmap"
+WEALTH_DISTRIBUTION_ACCIDENT_HEATMAP="2017 - Territorial Income Vs Number of Pedestrian Accidents"
 setwd(PATH)
 
 barcelona_coordinates <- c(left = LEFT,
@@ -38,7 +38,7 @@ barcelona_map=ggmap(barcelona)
 
 #read datasets
 barcelona_infrastructure = read_csv(INFRA_PATH)
-
+barcelona_infrastructure=na.omit(barcelona_infrastructure)
 barcelona_accidents = read_csv(ACCIDENTS)
 barcelona_pedestrian_accidents = barcelona_accidents[barcelona_accidents$DescriptionofAccident != "Not pedesterain",]
 
@@ -47,15 +47,19 @@ barcelona_unemployment = read_csv(UNEMPLOYMENT)
 
 crossing_plot=barcelona_map+
               geom_point(data=barcelona_infrastructure, 
-                         aes(x=barcelona_infrastructure$Longitud, y=barcelona_infrastructure$Latitud,color="crossing_color"),
-                         size=0.5)+
+                         aes(x=barcelona_infrastructure$Longitud, 
+                             y=barcelona_infrastructure$Latitud,
+                             color="crossing_color"),
+                         size=1,alpha=0.3)+
               geom_point(data=barcelona_pedestrian_accidents, 
-                         aes(x=barcelona_pedestrian_accidents$Longitude, y=barcelona_pedestrian_accidents$Latitude,color="accident_color"),
-                         size=0.5)+
+                         aes(x=barcelona_pedestrian_accidents$Longitude, 
+                             y=barcelona_pedestrian_accidents$Latitude,
+                             color="accident_color"),
+                         size=1)+
               ggtitle(ACCIDENT_VS_PEDESTRIAN_TITLE)+
               scale_color_manual("Representation", 
                      values = c(crossing_color="blue", accident_color="red"), 
-                     labels=c("Pedestrian Deaths","Pedestrian Crossings"))+
+                     labels=c("Pedestrian Accidents","Pedestrian Crossings"))+
   xlab(NULL)+
   ylab(NULL)+
   theme(plot.title = element_text(face="bold",hjust = 0.5),
@@ -69,27 +73,47 @@ crossing_plot=barcelona_map+
         panel.border = element_blank())
 crossing_plot
 ggsave(filename=paste(ACCIDENT_VS_PEDESTRIAN_TITLE,".jpg",sep=""),plot=last_plot(),
-       device="jpeg",path=PATH,width=12,height = 8)
+       device="jpeg",path=PATH,width=12,height = 8,dpi=300)
 
 #getting count of districts
 barcelona_pedestrian_accidents['Name of the district'] =lapply(barcelona_pedestrian_accidents['Name of the district'],factor)
 count_of_districts=as.data.frame(table(barcelona_pedestrian_accidents$`Name of the district`))
+count_of_districts_crossings=as.data.frame(table(barcelona_infrastructure$Nom_Districte))
+
 colnames(count_of_districts) <- c("District","Count of Accidents") 
-ggplot(data=count_of_districts, aes(District,`Count of Accidents`)) +
-  geom_bar(stat = "identity")+
+colnames(count_of_districts_crossings) <- c("District","Count of Crossings") 
+accidents_crossings=merge(count_of_districts, count_of_districts_crossings,
+                          by="District")
+accidents_crossings_melt <- melt(accidents_crossings,id.vars = 1)
+ggplot(accidents_crossings_melt,aes(x = District,y = value)) + 
+  geom_bar(aes(fill = variable),stat = "identity",position = "dodge")+
+  facet_wrap(~variable,scales = "free")+
+  scale_fill_manual(values = alpha(c("red", "blue"),c(1,0.8)))+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        plot.title = element_text(hjust = 0.5))+
   ggtitle(DISTRICT_PEDESTRIAN_COUNT)
+  
+# ggplot(data=count_of_districts, aes(District,`Count of Accidents`)) +
+#   geom_bar(stat = "identity")+
+#   ggtitle(DISTRICT_PEDESTRIAN_COUNT)
 ggsave(filename=paste(DISTRICT_PEDESTRIAN_COUNT,".jpg",sep=""),plot=last_plot(),
-       device="jpeg",path=PATH,width=12,height = 7)
+       device="jpeg",path=PATH,width=12,height = 7,dpi=300)
 
 #getting cause of accident
 barcelona_pedestrian_accidents['DescriptionofAccident'] = lapply(barcelona_pedestrian_accidents['DescriptionofAccident'],factor)
 count_of_cause=as.data.frame(table(barcelona_pedestrian_accidents$DescriptionofAccident))
 colnames(count_of_cause) <- c("Cause of Accident","Count of Accidents") 
-ggplot(data=count_of_cause, aes(`Cause of Accident`,`Count of Accidents`)) +
-  geom_bar(stat = "identity")+
-  ggtitle(PREDESTRIAN_CAUSE_COUNT)
+
+ggplot(data=count_of_cause, 
+       aes(reorder(`Cause of Accident`,`Count of Accidents`),
+           `Count of Accidents`)) +
+  geom_bar(stat = "identity",width=0.5, fill="#619CFF")+
+  xlab("Cause of Accidents")+
+  ylab("Count of Accidents")+
+  theme(plot.title = element_text(hjust = 0.5))+
+    ggtitle(PREDESTRIAN_CAUSE_COUNT)
 ggsave(filename=paste(PREDESTRIAN_CAUSE_COUNT,".jpg",sep=""),plot=last_plot(),
-       device="jpeg",path=PATH,width=12,height = 7)
+       device="jpeg",path=PATH,width=12,height = 7,dpi=300)
 
 #Day-Hour accident count
 barcelona_pedestrian_accidents['Day'] = lapply(barcelona_pedestrian_accidents['Day'],factor)
@@ -100,20 +124,13 @@ count_day_hour=rename(count(barcelona_pedestrian_accidents,
 colnames(count_day_hour) <- c("Day","Hour","Count of Accidents")
 count_day_hour['Hour']=lapply(count_day_hour['Hour'], factor)
 
-ggplot(data=count_day_hour,aes(x=Day,y=Hour))+
+ggplot(data=count_day_hour,aes(x=reorder(Day),y=Hour))+
   geom_point(aes(size = `Count of Accidents`))+
   scale_y_discrete(breaks = count_day_hour$Hour)+
-  ggtitle(DAY_HOUR_COUNT_1)
-ggsave(filename=paste(DAY_HOUR_COUNT_1,".jpg",sep=""),plot=last_plot(),
-       device="jpeg",path=PATH,width=12,height = 7)
-
-ggplot(data=count_day_hour,aes(x=Hour,y=`Count of Accidents`,col=Day))+
-  geom_point(aes(size = `Count of Accidents`))+
-  scale_y_continuous(breaks = count_day_hour$`Count of Accidents`)+
-  scale_x_discrete(breaks = count_day_hour$Hour)+
-  ggtitle(DAY_HOUR_COUNT_2)
-ggsave(filename=paste(DAY_HOUR_COUNT_2,".jpg",sep=""),plot=last_plot(),
-       device="jpeg",path=PATH,width=12,height = 7)
+  theme(plot.title = element_text(hjust = 0.5))+
+  ggtitle(DAY_HOUR_COUNT)
+ggsave(filename=paste(DAY_HOUR_COUNT,".jpg",sep=""),plot=last_plot(),
+       device="jpeg",path=PATH,width=12,height = 7,dpi=300)
 
 #Wealth per district
 barcelona_wealth['District Name'] = lapply(barcelona_wealth['District Name'],factor)
@@ -139,41 +156,79 @@ ggplot(data=district_wealth_unemployment,aes(x=`District Name`,y=`Mean Wealth`,c
                    values = c(below_average="red", above_average="green"), 
                    labels=c("Above Average","Below Average"))+
   ylab("Income Per Captia Index with Average=100")+
+  geom_hline(yintercept = 100, colour="black", linetype="dashed")+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
   ggtitle(INCOME_UNEMPLOYMENT_DISTRICT)
 ggsave(filename=paste(INCOME_UNEMPLOYMENT_DISTRICT,".jpg",sep=""),plot=last_plot(),
-       device="jpeg",path=PATH,width=12,height = 7)
+       device="jpeg",path=PATH,width=12,height = 7,dpi = 300)
 
-#Sankey Diagram Day-> Accidents -> Hour
 #Terrororial Wealth heatmap on plolygon coord
 barcelona_geojson=geojson_read("https://cdn.rawgit.com/martgnz/bcn-geodata/master/barris/barris_geo.json", 
                                what="sp")
 id_lookup=as.data.frame(barcelona_geojson@data)
 id_lookup$id=0:(nrow(id_lookup)-1)
 barcelona_polygon=fortify(barcelona_geojson)
-plot(barcelona_geojson)
+#plot(barcelona_geojson)
 barcelona_neighborhood_polygon=merge(barcelona_polygon, 
                                      id_lookup, by.x = "id", by.y = "id")
 barcelona_neighborhood_polygon=merge(barcelona_neighborhood_polygon,
                                      barcelona_wealth,
                                      by.x="name", by.y="Neighborhood Name")
 
-barcelona_map+
-  stat_density2d(data=barcelona_neighborhood_polygon, 
-                 mapping=aes(x=long, y=lat, fill=`Index RFD Barcelona = 100`),
-                 alpha=0.3, geom="polygon")
-
-
 barcelona_polygon=tidy(barcelona_geojson,region="C_Barri")
 barcelona_polygon$id=as.numeric(barcelona_polygon$id)
 fortified=barcelona_polygon%>%
   left_join(.,barcelona_wealth, by=c("id"="Neighborhood Code"))
 
-barcelona_map +
+ggplot()+
   geom_polygon(data = fortified, aes(fill = `Index RFD Barcelona = 100`,
                                      x = long, y = lat, group = group),alpha=0.8) +
   theme_void() +
+  scale_fill_viridis(trans = "log", breaks=c(50,75,100,125,150,175,200,225), 
+                     name="Income Per Captia Index", 
+                     guide = guide_legend( keyheight = unit(3, units = "mm"), 
+                                           keywidth=unit(12, units = "mm"), 
+                                           label.position = "bottom", 
+                                           title.position = 'top', nrow=1) ) +
+  theme(
+    text = element_text(color = "#22211d"), 
+    plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+    panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    
+    legend.position = "bottom"
+  ) +
   coord_map()+
   ggtitle(WEALTH_DISTRIBUTION_HEATMAP)
 
-#Check for sankey diag of District->Deaths->Wealth
-#Geo Heatmap of airquality and corresponding deaths in Nov 2017
+ggsave(filename=paste(WEALTH_DISTRIBUTION_HEATMAP,".jpg",sep=""),plot=last_plot(),
+       device="jpeg",path=PATH,width=12,height = 7,dpi = 300)
+
+barcelona_map +
+  geom_polygon(data = fortified, aes(fill = `Index RFD Barcelona = 100`,
+                                     x = long, y = lat, group = group),alpha=0.8) +
+  geom_point(data=barcelona_pedestrian_accidents, 
+             aes(x=barcelona_pedestrian_accidents$Longitude, 
+                 y=barcelona_pedestrian_accidents$Latitude),color="red",
+             size=1)+
+  theme_void() +
+  scale_fill_viridis(trans = "log", breaks=c(50,75,100,125,150,175,200,225), 
+                     name="Income Per Captia Index", 
+                     guide = guide_legend( keyheight = unit(3, units = "mm"), 
+                                           keywidth=unit(12, units = "mm"), 
+                                           label.position = "bottom", 
+                                           title.position = 'top', nrow=1) ) +
+  theme(
+    text = element_text(color = "#22211d"), 
+    plot.title = element_text(hjust = 0.5),
+    #plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+    panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    
+    legend.position = "bottom"
+  ) +
+  coord_map()+
+  ggtitle(WEALTH_DISTRIBUTION_ACCIDENT_HEATMAP)
+ggsave(filename=paste(WEALTH_DISTRIBUTION_ACCIDENT_HEATMAP,".jpg",sep=""),plot=last_plot(),
+       device="jpeg",path=PATH,width=12,height = 7,dpi = 300)
